04a445d9618c46ea9418f2fad089cb7cd19c8bcb
"yzc" <"yzcibrahim@gmail.com">
2022-01-12 22:25:24 +0300

    simple varaibles.

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -3,4 +3,17 @@ pragma solidity  >=0.7.0 <0.9.0;
 
 contract MyContract{
     string public myString = "Hello Solidity";
+    uint public myInt;
+
+    function setStr(string memory s) public {
+    myString=s;
+    }
+
+    function incrInt() public{
+        myInt++;
+    }
+
+    function decrInt() public{
+        myInt--;
+    }
 }
