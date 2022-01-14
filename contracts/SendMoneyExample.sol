4dd9a74d5b144f15705a26c442dd819fa56e2027
"yzc" <"yzcibrahim@gmail.com">
2022-01-14 14:37:08 +0300

    require owner and pause smart contract

--- contracts/SendMoneyExample.sol
+++ contracts/SendMoneyExample.sol
@@ -1,11 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity  >=0.7.0 <0.9.0;
 
 contract SendMoneyExample{
 
     uint public balanceReceived;
     uint public lockedUntil;
+    address public owner;
+    bool public paused;
+    constructor()
+    {
+        owner = msg.sender;
+    }
+
+    function setPaused(bool _paused) public{
+     require(msg.sender == owner,"you are not the owner");
+        paused=_paused;
+    }
 
     function receiveMoney() public payable {
+        require(paused==false,"The Contract is puased");
         balanceReceived += msg.value;
         lockedUntil = block.timestamp + 1 minutes;
     }
@@ -19,13 +32,17 @@ contract SendMoneyExample{
     }
 
     function withdrawMoney() public{
+        require(msg.sender == owner,"you are not the owner");
         require(lockedUntil < block.timestamp,"it is still blocked");
+        require(paused==false,"The Contract is puased");
         payable(msg.sender).transfer(getBalance());
         balanceReceived=0;
     }
 
     function withdrawMoneyTo(address payable _to) public{
+        require(msg.sender == owner,"you are not the owner");
         require(lockedUntil < block.timestamp,"it is still blocked");
+        require(paused==false,"The Contract is puased");
         _to.transfer(this.getBalance());
         balanceReceived=0;
     }
